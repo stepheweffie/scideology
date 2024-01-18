dfe843ff76111c7781c20a5a6c6f4a87b6b87a71
stepheweffie <stephie.maths@gmail.com>
2024-01-18 17:41:49 -0500

    menu items

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from nicegui import ui
 from settings import *
+# Every .py file is on a microservice
 
 
 async def page_content(pagename: str):
-    global page_dict
     desc = ui.label(f'{page_dict[pagename.lower()]}').style(replace=f'font-family: {sans_serif};'
                                                                     f'color: {font_color}').classes('text-xl')
     desc.classes('animate__animated animate__fadeInDown')
@@ -32,10 +32,12 @@ async def page_footer(pagename: str):
                                                       f'color: {font_color}')
 
 
+def menu_list(page_list_item):
+    item = ui.menu_item(f'{page_list_item}', lambda: ui.open(f'{page_list_item.lower()}'), auto_close=True)
+    item.classes('text-xl')
+
+
 async def page_menu(pagename: str):
-    def menu_list(page_list_item):
-        item = ui.menu_item(f'{page_list_item}', lambda: ui.open(f'{page_list_item.lower()}'), auto_close=True)
-        item.classes('text-xl')
 
     with ui.row().classes('w-full h-full no-wrap top-0 left-0'):
         with ui.button(icon='menu', color='#737373').classes('absolute top-0 right-0 m-3 p-3 rounded-full'):
@@ -81,7 +83,7 @@ async def main():
             with ui.menu() as menu:
                 menu.classes(f'text-xl')
                 for page in pages:
-                   ui.menu_item(f'{page}', lambda: ui.open(f'{page.lower()}'), auto_close=True)
+                    menu_list(page)
         ui.query('body').style(replace=f'background-color: {bg_color};')
         ui.image('static/images/Home.svg').style(replace='width: 100%; height: 100%;')
         ui.label(f'{main_page_data}').style(replace=f'font-family: {main_font}; font-size: {main_font_size}; '
